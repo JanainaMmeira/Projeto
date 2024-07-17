48cb18caa125bc8d7cbe1cc8406243f22e3c542b
Janaina <janainamcamara@hotmail.com>
2024-07-17 08:21:23 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // Pegar a tag image
+  const img = document.querySelector("#profile img")
+//subistituir a imagens
+  if(body.classList.constains('light')) {
+  //se tiver no light mode, manter a imagem light
+    img.setAttribute('src', './Assets/avatar-light.png')
+  } else {
+  // se tiver sem light mode, manter a imagem comum.
+    img.setAttribute('src', './Assets/avatar.png')
+}
+
+}
